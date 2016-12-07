43674bf944a066177be3beb75531c35f2d6bbc7e
Jesper Sørensen <Sorensen_hnd90@hotmail.com>
2016-12-07 21:38:31 +0100

    update

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module SmolovCal
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_dispatch.default_headers = {
-    'X-Frame-Options' => 'ALLOW-FROM https://quiet-shelf-52098.herokuapp.com/'
+    'X-Frame-Options' => 'ALLOW-FROM http://smolovcal.com'
     }
   end
 end
