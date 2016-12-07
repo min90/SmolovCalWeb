e64ef39d00c681f5d9461f1979baf0ab99fe1792
Jesper Sørensen <Sorensen_hnd90@hotmail.com>
2016-12-07 09:00:37 +0100

    Added iFrame support

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
+  end
+
 end
