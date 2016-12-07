7980de54ab2cc5c7f22dde2ff2b3a799a3ef5d46
Jesper Sørensen <Sorensen_hnd90@hotmail.com>
2016-12-07 22:04:43 +0100

    update4

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module SmolovCal
     SecureHeaders::Configuration.default do |config|
     	config.csp = {
     		default_src: %w(https: 'self'),
-    		frame_ancestors: %w(http://smolovcal.com)
+    		frame_ancestors: %w('self http://smolovcal.com)
     	}
     end
   end
